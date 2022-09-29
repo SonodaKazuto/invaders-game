f6327aac54cc1f1c54891f8f9d2217fb7ae3c024
SonodaKazuto <86616409+SonodaKazuto@users.noreply.github.com>
2022-09-30 02:50:08 +0800

    finished part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::io;
+
+use crossterm::{ExecutableCommand, terminal};
+use crossterm::cursor::{Hide, Show};
+use crossterm::style::Attribute::Hidden;
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use rusty_audio::Audio;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+    audio.add("explode", "sounds/explode.wav");
+    audio.add("lose", "sounds/lose.wav");
+    audio.add("move", "sounds/move.wav");
+    audio.add("pew", "sounds/pew.wav");
+    audio.add("startup", "sounds/startup.wav");
+    audio.add("win", "sounds/win.wav");
+    audio.play("startup");
+
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Cleanup
+    audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    Ok(())
 }
+
